bd60bb9027bab2b20e21704b718cb613458010a9
Alberto Mario Consuegra Laino <am.consuegra10@uniandes.edu.co>
2015-09-06 17:28:17 -0500

    Cambios basados en book

--- src/kissis.web/src/main/java/co/edu/uniandes/kissis/service/PacienteServices.java
+++ src/kissis.web/src/main/java/co/edu/uniandes/kissis/service/PacienteServices.java
@@ -1,10 +1,10 @@
 package co.edu.uniandes.kissis.service;
 
+import co.edu.uniandes.kissis.api.IPacienteLogic;
 import co.edu.uniandes.kissis.dtos.PacienteDTO;
-import co.edu.uniandes.kissis.ejb.IPacienteLogic;
+import co.edu.uniandes.kissis.providers.StatusCreated;
 import java.util.List;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,7 +13,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -25,51 +24,41 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class PacienteServices
 {
-    @Context 
-    private HttpServletResponse response;
-    
     @Inject
     private IPacienteLogic pacienteLogic;
-    
-    /**
-     * Metodo POST para la creación de un paciente
-     * @param dto
-     * @return 
-     */
+
     @POST
-    public PacienteDTO createPaciente(PacienteDTO dto) 
+    @StatusCreated
+    public PacienteDTO createPaciente(PacienteDTO dto)
     {
         return pacienteLogic.createPaciente(dto);
     }
-    
-    /**
-     * Metodo GET para obtener los libros
-     * @return 
-     */
+
     @GET
-    public List<PacienteDTO> getPacientes() 
+    public List<PacienteDTO> getPacientes()
     {
         return pacienteLogic.getPacientes();
     }
-    
-     /**
-     * Metodo PUT utilizado principalmente para actualizar
-     * @return 
-     */
+
+    @GET
+    @Path("{id: \\d+}")
+    public PacienteDTO getPaciente(@PathParam("id") Long id)
+    {
+        return pacienteLogic.getPaciente(id);
+    }
+
     @PUT
     @Path("{id: \\d+}")
-    public PacienteDTO updatePaciente(@PathParam("tipoId") String pTipoId, @PathParam("id") String pId, PacienteDTO dto) 
+    public PacienteDTO updatePaciente(@PathParam("id") Long id, PacienteDTO dto)
     {
-        return pacienteLogic.updatePaciente(pTipoId,pId,dto);
+        dto.setId(id);
+        return pacienteLogic.updatePaciente(dto);
     }
-    
-    /**
-     * Metodo DELETE usado para eliminar un paciente
-     */
+
     @DELETE
     @Path("{id: \\d+}")
-    public void deletePaciente(@PathParam("tipoId") String pTipoId, @PathParam("id") String pId)
+    public void deleteBook(@PathParam("id") Long id)
     {
-        pacienteLogic.deletePaciente(pTipoId,pId);
+        pacienteLogic.deletePaciente(id);
     }
 }
