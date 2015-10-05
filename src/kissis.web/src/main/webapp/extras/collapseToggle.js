c4f25f1890ee203f88905431338bdfe6029cd161
Camila Garcia Hernandez <c.garcia11@uniandes.edu.co>
2015-10-05 12:57:00 -0500

    Borrar librerias

--- src/kissis.web/src/main/webapp/extras/collapseToggle.js
+++
@@ -1,4 +0,0 @@
-angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('CollapseDemoCtrl', function ($scope) {
-  $scope.isCollapsed = false;
-});
