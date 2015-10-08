44d8af62ffdb5cf9bc7bcff7c0cd67452107e1fd
Alberto Consuegra <am.consuegra10@uniandes.edu.co>
2015-10-07 22:17:52 -0500

    relaciones

--- src/kissis.logic/src/main/java/co/edu/uniandes/kissis/api/IPacienteLogic.java
+++ src/kissis.logic/src/main/java/co/edu/uniandes/kissis/api/IPacienteLogic.java
@@ -1,5 +1,6 @@
 package co.edu.uniandes.kissis.api;
 
+import co.edu.uniandes.kissis.dtos.CitaDTO;
 import co.edu.uniandes.kissis.dtos.PacienteDTO;
 import java.util.List;
 
@@ -14,4 +15,9 @@ public interface IPacienteLogic
     public PacienteDTO createPaciente(PacienteDTO dto);
     public PacienteDTO updatePaciente(PacienteDTO dto);
     public void deletePaciente(Long id);
+    public CitaDTO addCita(Long citaId, Long pacienteId);
+    public void removeCita(Long citaId, Long pacienteId);
+    public List<CitaDTO> replaceCitas(List<CitaDTO> citas, Long pacienteId);
+    public List<CitaDTO> getCitas(Long pacienteId);
+    public CitaDTO getCita(Long pacienteId, Long citaId);
 }
