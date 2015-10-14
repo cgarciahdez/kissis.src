da334f0a2ce45bf132930528c336bc36f5c87150
Camila Garcia Hernandez <c.garcia11@uniandes.edu.co>
2015-10-14 13:15:50 -0500

    Adelantos en req. Front todavia no funcional.

--- src/kissis.logic/src/main/java/co/edu/uniandes/kissis/api/ICitaLogic.java
+++ src/kissis.logic/src/main/java/co/edu/uniandes/kissis/api/ICitaLogic.java
@@ -1,8 +1,11 @@
 package co.edu.uniandes.kissis.api;
 
+import co.edu.uniandes.kissis.dtos.CitaDTO;
 import java.util.List;
 
 public interface ICitaLogic {
+    
+    public CitaDTO createCita(CitaDTO dto);
 
 //    public List<AuthorDTO> getAuthors();
 //
--- src/kissis.web/src/main/webapp/src/modules/doctor/doctor.ctrl.js
+++ src/kissis.web/src/main/webapp/src/modules/doctor/doctor.ctrl.js
@@ -1,16 +1,33 @@
 (function (ng) {
     var mod = ng.module('doctorModule');
 
-    mod.controller('doctorCtrl', ['$scope', 'doctorService', 'citaService', function ($scope, svc) {
+    mod.controller('doctorCtrl', ['$scope', 'doctorService',  function ($scope, svc) {
             $scope.currentRecord = {};
             $scope.records = [];
+            
+            var horas = [{"horaInicio":"7:00"},{"horaInicio":"8:00"},{"horaInicio":"9:00"},
+            {"horaInicio":"10:00"},{"horaInicio":"11:00"},{"horaInicio":"12:00"},{"horaInicio":"13:00"},
+            {"horaInicio":"14:00"},{"horaInicio":"15:00"},{"horaInicio":"16:00"},{"horaInicio":"17:00"},
+            {"horaInicio":"18:00"}];
+        
+            $scope.times=horas;
 
             //Variables para el controlador
-            this.readOnly = true;
+            this.readOnly = false;
             this.editMode = false;
             this.verPerfil = false;
             this.createMode = true;
+            this.seleccionarHorarios = false;
             var self = this;
+            
+            this.verHorarios = function ()
+            {
+               self.createMode = false;
+               self.readOnly = false;
+               self.editMode = false;
+               self.verPerfil = false;
+               self.seleccionarHorarios = true;
+            };
             
             this.mostrarDoctores = function ()
             {
@@ -70,5 +87,93 @@
             };
 
             this.fetchRecords();
+            
+          
+            $scope.today = function() {
+            $scope.dt = new Date();
+            };
+            $scope.today();
+
+            $scope.clear = function() {
+            $scope.dt = null;
+            };
+
+            // Disable weekend selection
+            $scope.disabled = function(date, mode) {
+            return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+            };
+
+            $scope.toggleMin = function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+            };
+            $scope.toggleMin();
+
+            $scope.open = function($event) {
+            $scope.status.opened = true;
+             };
+
+  $scope.variable = false;
+  $scope.mostrar=0;
+
+  $scope.$watch('dt', function() {
+    if($scope.mostrar===0)
+    {
+      $scope.mostrar=1;
+    }
+    else{
+    $scope.variable = true;
+    var days = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+    $scope.nomDia = days[$scope.dt.getDay()] + " " + $scope.dt.getDate();
+    }
+  });
+
+  $scope.mostrarDia = function() {
+    $scope.variable = true;
+    var days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    $scope.nomDia = days[$scope.dt.getDay()] + " " + $scope.dt.getDate();
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+
+  $scope.status = {
+    opened: false
+  };
+
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var afterTomorrow = new Date();
+  afterTomorrow.setDate(tomorrow.getDate() + 2);
+  var holi = new Date("October 13, 2015 11:13:00");
+  $scope.events = [{
+    date: tomorrow,
+    status: 'full'
+  }, {
+    date: afterTomorrow,
+    status: 'partially'
+  } ];
+
+  $scope.getDayClass = function(date, mode) {
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+
+    return '';
+  };
         }]);
+    
+    
 })(window.angular);
