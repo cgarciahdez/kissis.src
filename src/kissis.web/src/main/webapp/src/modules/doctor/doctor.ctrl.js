0256494d6b6116152048e446db14810c19cb57f9
Camila Garcia Hernandez <c.garcia11@uniandes.edu.co>
2015-10-22 10:14:35 -0500

    Revert

--- src/kissis.web/src/main/webapp/src/modules/doctor/doctor.ctrl.js
+++ src/kissis.web/src/main/webapp/src/modules/doctor/doctor.ctrl.js
@@ -1,16 +1,16 @@
 (function (ng) {
     var mod = ng.module('doctorModule');
 
-    mod.controller('doctorCtrl', ['$scope', 'doctorService',  function ($scope, svc) {
+    mod.controller('doctorCtrl', ['$scope', 'doctorService', function ($scope, svc) {
             $scope.currentRecord = {};
             $scope.records = [];
-            
-            var horas = [{"horaInicio":7},{"horaInicio":8},{"horaInicio":9},
-            {"horaInicio":10},{"horaInicio":11},{"horaInicio":12},{"horaInicio":13},
-            {"horaInicio":14},{"horaInicio":15},{"horaInicio":16},{"horaInicio":17},
-            {"horaInicio":18}];
-        
-            $scope.times=horas;
+
+            var horas = [{"horaInicio": 7}, {"horaInicio": 8}, {"horaInicio": 9},
+                {"horaInicio": 10}, {"horaInicio": 11}, {"horaInicio": 12}, {"horaInicio": 13},
+                {"horaInicio": 14}, {"horaInicio": 15}, {"horaInicio": 16}, {"horaInicio": 17},
+                {"horaInicio": 18}];
+
+            $scope.times = horas;
 
             //Variables para el controlador
             this.readOnly = false;
@@ -19,38 +19,38 @@
             this.createMode = true;
             this.seleccionarHorarios = false;
             var self = this;
-            
+
             this.verHorarios = function ()
             {
-               self.createMode = false;
-               self.readOnly = false;
-               self.editMode = false;
-               self.verPerfil = false;
-               self.seleccionarHorarios = true;
+                self.createMode = false;
+                self.readOnly = false;
+                self.editMode = false;
+                self.verPerfil = false;
+                self.seleccionarHorarios = true;
             };
-            
-            this.getSelected = function()
+
+            this.getSelected = function ()
             {
                 var t;
                 for (t in $scope.times)
                 {
                     if (t.selected)
                     {
-                        var cita = {};
-                        svc
+                        var cita = {"consultorio": 1};
+                        cita={"consultorio": 1};
                     }
                 }
             };
-            
+
             this.mostrarDoctores = function ()
             {
-               self.createMode = false;
-               self.readOnly = true;
-               self.editMode = false;
-               self.verPerfil = false;
+                self.createMode = false;
+                self.readOnly = true;
+                self.editMode = false;
+                self.verPerfil = false;
             };
-            
-            
+
+
             this.createRecord = function () {
                 self.editMode = true;
                 self.verPerfil = true;
@@ -100,93 +100,81 @@
             };
 
             this.fetchRecords();
-            
-          
-            $scope.today = function() {
-            $scope.dt = new Date();
+
+
+            $scope.today = function () {
+                $scope.dt = new Date();
             };
             $scope.today();
 
-            $scope.clear = function() {
-            $scope.dt = null;
+            $scope.clear = function () {
+                $scope.dt = null;
             };
 
             // Disable weekend selection
-            $scope.disabled = function(date, mode) {
-            return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+            $scope.disabled = function (date, mode) {
+                return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
             };
 
-            $scope.toggleMin = function() {
-            $scope.minDate = $scope.minDate ? null : new Date();
+            $scope.toggleMin = function () {
+                $scope.minDate = $scope.minDate ? null : new Date();
             };
             $scope.toggleMin();
 
-            $scope.open = function($event) {
-            $scope.status.opened = true;
-             };
+            $scope.open = function () {
+                $scope.status.opened = true;
+            };
 
-  $scope.variable = false;
-  $scope.mostrar=0;
+            $scope.variable = false;
+            $scope.mostrar = 0;
 
-  $scope.$watch('dt', function() {
-    if($scope.mostrar===0)
-    {
-      $scope.mostrar=1;
-    }
-    else{
-    $scope.variable = true;
-    var days = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
-    $scope.nomDia = days[$scope.dt.getDay()] + " " + $scope.dt.getDate();
-    }
-  });
+            $scope.$watch('dt', function () {
+                if ($scope.mostrar === 0)
+                {
+                    $scope.mostrar = 1;
+                }
+                else {
+                    $scope.variable = true;
+                    var days = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+                    $scope.nomDia = days[$scope.dt.getDay()] + " " + $scope.dt.getDate();
+                }
+            });
 
-  $scope.mostrarDia = function() {
-    $scope.variable = true;
-    var days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-    $scope.nomDia = days[$scope.dt.getDay()] + " " + $scope.dt.getDate();
-  };
+            $scope.mostrarDia = function () {
+                $scope.variable = true;
+                var days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+                $scope.nomDia = days[$scope.dt.getDay()] + " " + $scope.dt.getDate();
+            };
 
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
-  };
+            $scope.dateOptions = {
+                formatYear: 'yy',
+                startingDay: 1
+            };
 
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
+            $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+            $scope.format = $scope.formats[0];
 
-  $scope.status = {
-    opened: false
-  };
+            $scope.status = {
+                opened: false
+            };
 
-  var tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  var afterTomorrow = new Date();
-  afterTomorrow.setDate(tomorrow.getDate() + 2);
-  var holi = new Date("October 13, 2015 11:13:00");
-  $scope.events = [{
-    date: tomorrow,
-    status: 'full'
-  }, {
-    date: afterTomorrow,
-    status: 'partially'
-  } ];
 
-  $scope.getDayClass = function(date, mode) {
-    if (mode === 'day') {
-      var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+            $scope.getDayClass = function (date, mode) {
+                if (mode === 'day') {
+                    var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
 
-      for (var i = 0; i < $scope.events.length; i++) {
-        var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+                    for (var i = 0; i < $scope.events.length; i++) {
+                        var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
 
-        if (dayToCheck === currentDay) {
-          return $scope.events[i].status;
-        }
-      }
-    }
+                        if (dayToCheck === currentDay) {
+                            return $scope.events[i].status;
+                        }
+                    }
+                }
 
-    return '';
-  };
+                return '';
+            };
         }]);
-    
-    
+
+
 })(window.angular);
